14ef87cb3c30b3fd05fa6e53c013dfa22d9bf1a3
Kevin-2002 <kevinmcshane02@gmail.com>
2023-09-29 11:43:46 +0100

    lab2

---
+++ lab2/lab2.js
@@ -0,0 +1,12 @@
+const SayHello = function Hello(){
+    console.log("Hello World");
+}
+
+SayHello();
+
+const arrow = (myVal)=>{
+    return myVal
+}
+
+arrow("Data Representation & Querying");
+console.log(arrow("Data Representation & Querying"));
